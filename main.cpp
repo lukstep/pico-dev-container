ee79151c76483f37f1e4fed9ef4d6df2f926578d
lukstep <lukaszstepien02@gmail.com>
2024-06-26 19:34:59 +0200

    Resolve problem with debuging via openOCD

--- main.cpp
+++ main.cpp
@@ -1,12 +1,22 @@
 #include "hardware/gpio.h"
 #include "pico/binary_info.h"
 #include "pico/stdlib.h"
+#include "hardware/timer.h"
 #include <iostream>
+
+void enable_debug()
+{
+#ifndef NDEBUG
+    // workaround for CMSIS-DAP,
+    // see: https://github.com/raspberrypi/pico-sdk/issues/1152
+    timer_hw->dbgpause = 0;
+#endif
+}
 
 int main ()
 {
+    enable_debug();
     stdio_init_all();   
-    
     constexpr int LED_PIN = 25;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
@@ -15,7 +25,7 @@ int main ()
     {
         gpio_put(LED_PIN, 0);
         sleep_ms(250);
-        gpio_put(LED_PIN, 1);
+        gpio_put(LED_PIN, 1)
         std::cout << "Hello Word\n";
         sleep_ms(1000);
     }
